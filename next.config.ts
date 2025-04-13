497227f500e3a55ff054770ad02cfe2fd0bb28b0
Ravi Riley <raviriley@gmail.com>
2025-04-13 14:01:44 +0900

    Revert "update config"
    This reverts commit 125d3d727f788b1afdcb0056552b6bc832907509.

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.externals = [
-      ...(config.externals || []),
-      "pino-pretty",
-      "lokijs",
-      "encoding",
-    ];
-    return config;
-  },
 };
 
 export default nextConfig;
